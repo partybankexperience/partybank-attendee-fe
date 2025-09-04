914a1093dee533b2827230118592c4326bb96450
Jonathan Martins <mart1449@algonquinlive.com>
2025-09-03 21:34:03 -0400

    to be disclosed added

--- src/pages/EventDetails/EventDetailContainer.tsx
+++ src/pages/EventDetails/EventDetailContainer.tsx
@@ -223,7 +223,7 @@ const EventDetails: React.FC = () => {
 
                       <div className="flex items-center gap-3">
                         <MapPin className="w-5 h-5 text-primary/80 shrink-0" />
-                        <span>{eventDetail.venueName || "TBD"}</span>
+                        <span>{eventDetail.venueName || "To Be Disclosed"}</span>
                       </div>
                     </div>
 
